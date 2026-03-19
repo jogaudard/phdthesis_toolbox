7427598c13160acb8d71a4e52cfb742da76a5e88
jogaudard <joseph.gaudard@pm.me>
2026-03-19 16:30:48 +0100

    moving author tbl into a target

---
+++ R/authors_tbl.R
@@ -0,0 +1,55 @@
+auhtors_tbl <- function(){
+   
+# paper_author_lists = c()
+paper1_authors <- read_sheet(gs4_find("authors_paper1"))
+
+aut_paper1 <- Plume$new(paper1_authors)
+contributions_paper1 <- aut_paper1$get_contributions(
+  dotted_initials = FALSE,
+  sep_last = ", "
+) |>
+c()
+
+paper2_authors <- read_sheet(gs4_find("authors_paper2"))
+
+aut_paper2 <- Plume$new(paper2_authors)
+contributions_paper2 <- aut_paper2$get_contributions(
+  dotted_initials = FALSE,
+  sep_last = ", "
+) |>
+c()
+
+contrib_paper <- function(
+  aut_vec, # a vector of author contributions as produced with aut$get_contributions() |> c()
+  paper_name, # name of the paper
+  split = ": " # same as divider in get_contributions
+){
+lapply(aut_vec, string_to_table, n = 2, split_regex = split) |>
+  bind_rows() |>
+  rename(
+    Role = "X1",
+    {{paper_name}} := "X2"
+  )
+}
+
+
+
+
+contrib_paper1 <- contrib_paper(
+  contributions_paper1,
+  "Paper I"
+)
+
+contrib_paper2 <- contrib_paper(
+  contributions_paper2,
+  "Paper II"
+)
+
+contrib_tbl <- full_join(
+  contrib_paper1,
+  contrib_paper2,
+  by = "Role"
+)
+
+contrib_tbl
+}
--- _targets.R
+++ _targets.R
@@ -48,16 +48,15 @@ tar_option_set(
 )
 
 # Run the R scripts in the R/ folder with your custom functions:
-# tar_source()
+tar_source()
 # tar_source("other_functions.R") # Source other scripts as needed.
 
 # Replace the target list below with your own:
 list(
-  # tar_target( # checking if the phd paper list is up to date
-  #   phd_papers,
-  #   "phd_papers.bib",
-  #   format = "file"
-  # ),
+  tar_target(
+    name = authors_tbl,
+    command = authors_tbl()
+  ),
   tar_quarto(
     name = render_paper_list,
     path = "paper_list.qmd",
